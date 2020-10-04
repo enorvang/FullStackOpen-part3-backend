626fddfce6fe594888d93d9d0a485208806e5ae8
enorvang <espen.norvang@gmail.com>
2020-10-04 18:54:29 +0200

    indentation

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const requestLogger = (request, response, next) => {
@@ -129,7 +131,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
