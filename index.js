9a0dcf7128eed1c980c0fd368738c3b1dd93489c
enorvang <espen.norvang@gmail.com>
2020-10-04 14:26:40 +0200

    added module support

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-import express from 'express';
+import express from "express";
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1> Hello World! </h1>");
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
