fe512de25c3a6d11a1de90babad8980bf9d77720
enorvang <espen.norvang@gmail.com>
2020-10-04 19:00:45 +0200

    uibuild

---
+++ build/precache-manifest.14c5b97d8089b450c3614cfd4e6d76cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e453131cbda7d3cdcb2a6b1073db11a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10445e24106af7a87563",
+    "url": "/static/css/main.b7e135b2.chunk.css"
+  },
+  {
+    "revision": "81d4ffa2ec2a86f93651",
+    "url": "/static/js/2.f49266f7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.f49266f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10445e24106af7a87563",
+    "url": "/static/js/main.6fb886b3.chunk.js"
+  },
+  {
+    "revision": "c6e3f79bc34f30ee080a",
+    "url": "/static/js/runtime-main.4db9dc37.js"
+  }
+]);
