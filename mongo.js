6665ec11bc145e00b9a5f6159e4ce3a78bc24d61
enorvang <espen.norvang@gmail.com>
2020-10-05 14:15:28 +0200

    completed exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password> "
+  );
+}
+
+const password = process.argv[2];
+const dbname = "phonebook-app";
+
+const url = `mongodb+srv://enorvang:${password}@fullstackopencluster.6hp2r.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length > 3) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${result.name}, number=${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
